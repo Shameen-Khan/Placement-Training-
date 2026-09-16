bafb1520c7d9613d0de242670386db4b2079130a
J SHAMEENKHAN <shameenkhan.j_25am@kgkite.ac.in>
2026-09-16 21:43:23 +0530

    Add even and odd index element programs

---
+++ 41_print_even_index_element.py
@@ -0,0 +1,4 @@
+a = [10, 20, 30, 40, 50]
+for i in range(0, len(a), 2):
+    print("Even element = ", a[i])
+    
---
+++ 42_print_odd_index_element.py
@@ -0,0 +1,4 @@
+a = [10, 20, 30, 40, 50]
+for i in range(1, len(a), 2):
+    print("Even element = ", a[i])
+    
